ffbf8c4331d2f709d05d6cda13515c5860c20236
zireal23 <skasif232000@gmail.com>
2023-03-10 22:09:27 +0530

    initial socket connection

--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,42 @@
 import express, { Express, Request, Response } from 'express';
+import { createServer } from "http";
+import { Server } from "socket.io";
 import dotenv from 'dotenv';
 import path from 'path';
-
+import cors from 'cors';
 dotenv.config();
 
-const app: Express = express();
-const port = process.env.PORT??8080;
-
+const app = express();
+const port = process.env.PORT ?? 8080;
+const server = createServer(app);
+const io = new Server(server, {
+  cors: {
+    origin: '*',
+    methods: ['GET', 'POST'],
+  },
+});
+app.use(cors({
+  origin: '*',
+  methods: ['GET', 'POST', 'PUT', 'DELETE'],
+}));
 app.use('/', express.static(path.join(__dirname, '../public')))
 
 app.get('/api', (req: Request, res: Response) => {
   res.send('Express + TypeScript Server');
 });
 
-app.listen(port, () => {
+io.on('connect', (socket) => {
+  console.log('user connected with id: ', socket.id);
+});
+
+io.on("connect_error", (err) => {
+  console.log(`connect_error due to ${err.message}`);
+});
+
+// app.listen(port, () => {
+//   console.log(`⚡️[server]: Server is running at http://localhost:${port}`);
+// });
+
+server.listen(port, () => {
   console.log(`⚡️[server]: Server is running at http://localhost:${port}`);
 });
