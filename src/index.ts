bcbbad3b66da52d867f68a10184d45fa39ea913b
zireal23 <skasif232000@gmail.com>
2023-03-11 11:04:41 +0530

    implemented basic key exchange

--- src/index.ts
+++ src/index.ts
@@ -9,6 +9,7 @@ dotenv.config();
 const app = express();
 const port = process.env.PORT ?? 8080;
 const server = createServer(app);
+let prekey:string;
 const io = new Server(server, {
   cors: {
     origin: '*',
@@ -27,11 +28,24 @@ app.get('/api', (req: Request, res: Response) => {
 
 io.on('connect', (socket) => {
   console.log('user connected with id: ', socket.id);
+  socket.join("chatRoom");
+  socket.emit('usersInRoom',io.sockets.adapter.rooms.get("chatRoom")?.size);
+  socket.on("prekey", (data) => {
+    prekey = data;
+    console.log(`Prekey: ${prekey}`);
+  });
+  socket.on("sendprekey", (data) => { 
+    console.log(`Message from client: ${data}`);
+    socket.emit("keyExchange", prekey);
+  });
 });
 
 io.on("connect_error", (err) => {
   console.log(`connect_error due to ${err.message}`);
 });
+
+
+
 
 // app.listen(port, () => {
 //   console.log(`⚡️[server]: Server is running at http://localhost:${port}`);
