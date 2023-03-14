9a7b8feaeb2e68be69e298fc5098632ef7a3460e
Ronak Sanpui <ronaksanpui@gmail.com>
2023-03-14 19:27:02 +0530

    added typesafety for parameters, and added additional tests for  username and password

--- src/controllers/registerController.ts
+++ src/controllers/registerController.ts
@@ -2,6 +2,7 @@ import { Router } from "express";
 import emailvalidator from "email-validator";
 import { checkIfEmailExists as doesEmailAlreadyExists, insertUser as insertUserIntoDatabase } from "../databaseUtil";
 import bcrypt from "bcrypt";
+import passwordValidator from "password-validator";
 
 
 const router = Router();
@@ -16,6 +17,9 @@ router.post("/", async (req, res) => {
     if (email === undefined) {
         return res.status(400).send("Email is a required field");
     }
+    else if (typeof email !== "string") {
+        return res.status(400).send("Email must be of type string");
+    }
     else if (!emailvalidator.validate(email)) {
         return res.status(400).send("This email is not valid");
     }
@@ -23,28 +27,45 @@ router.post("/", async (req, res) => {
         return res.status(409).send("An account with this email already exists");
     }
 
-
-    const username = req.body.username;
+    const username: string | undefined = req.body.username;
+    const usrenameRegex = /^[a-zA-Z][a-zA-Z0-9\-_\.]{2,99}$/;
     if (username === undefined) {
         return res.status(400).send("Username is a required field");
     }
-    else if (username.length < 3) {
-        return res.status(400).send("Username must be at least 3 characters long");
+    else if (typeof username !== "string") {
+        return res.status(400).send("Username must be of type string");
+    }
+    else if (!usrenameRegex.test(username)) {
+        return res.status(400).send("Username must be at least 3 characters long, start with a letter, and only contain letters, numbers, dashes, underscores and dots");
     }
 
+    const password: string | undefined = req.body.password;
+    const passwordSchema = new passwordValidator();
+    passwordSchema
+        .min(8, "Password must be at least 8 characters long")
+        .max(100, "Password must be at most 100 characters long")
 
-    const password = req.body.password;
     if (password === undefined) {
         return res.status(400).send("Password is a required field");
     }
-    else if (password.length < 8) {
-        return res.status(400).send("Password must be at least 8 characters long");
+    else if (typeof password !== "string") {
+        return res.status(400).send("Password must be of type string");
+    }
+    else if (!passwordSchema.validate(password)) {
+        const errorMessageListWithDetails = passwordSchema.validate(password, { details: true }) as any[];
+        return res.status(400).send(errorMessageListWithDetails[0].message);
     }
 
     //Store the user in the database
     const hashedPassword = await bcrypt.hash(password, 10);
 
-    await insertUserIntoDatabase(username, email, hashedPassword);
+    try {
+        await insertUserIntoDatabase(username, email, hashedPassword);
+    }
+    catch (error) {
+        console.error(error);
+        return res.status(500).send("Something went wrong while trying to register the user");
+    }
 
     return res.send("User registered successfully!");
 });
